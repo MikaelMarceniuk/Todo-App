8a8297ec5bad13535b033cd6cc5864de545c169b
Mikael Marceniuk <mika.marceniuk@gmail.com>
2023-12-24 09:46:46 -0300

    feat: Created customCheckbox

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CustomCheckbox from '@/components/customCheckbox'
 import Header from '@/components/header'
 
 const HomePage = () => {
@@ -5,6 +6,13 @@ const HomePage = () => {
     <main className="min-h-screen px-7 py-12 bg-[#171823] bg-dark-mobile bg-contain bg-no-repeat">
       <div className="h-full">
         <Header />
+        <div className="w-full h-12 bg-[#25273D] rounded-md flex gap-3 px-5">
+          <CustomCheckbox />
+          <input
+            className="bg-[#25273D]"
+            placeholder="Create a new todo..."
+          />
+        </div>
       </div>
     </main>
   )
