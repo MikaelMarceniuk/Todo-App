38245727670f391793d902a995ed87ae9c07d6fb
Mikael Marceniuk <mika.marceniuk@gmail.com>
2023-12-24 10:18:54 -0300

    feat: Added spacing to "Create new Todo"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const HomePage = () => {
     <main className="min-h-screen px-7 py-12 bg-[#171823] bg-dark-mobile bg-contain bg-no-repeat">
       <div className="h-full">
         <Header />
-        <div className="w-full h-12 px-5 bg-[#25273D] rounded-md flex items-center gap-3 ">
+        <div className="w-full h-12 px-5 mt-10 bg-[#25273D] rounded-md flex items-center gap-3 ">
           <CustomCheckbox />
           <input
             className="bg-[#25273D] text-[#C8CBE7] text-xs focus:outline-none"
